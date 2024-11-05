b436f14fec9d49ead47d8fa2216ae476a9f6ba90
kent2980 <kent2980@gmail.com>
2024-11-05 18:36:34 +0900

    today_insert.pyにロックファイル機能を追加し、前回のプロセスが実行中の場合は処理をスキップするように更新

--- app/today_insert.py
+++ app/today_insert.py
@@ -1,12 +1,30 @@
 from datetime import datetime
+import os
+import sys
 
 from app.api.insert import Insert
 
+# ロックファイルのパスを指定
+lock_file = "/home/kent2980/docker_cont/stock-link-parser/script.lock"
+
 if __name__ == "__main__":
-    outputPath = (
-        "/Users/user/Vscode/XBRL_Parse_Project/stock-link-parser/output"
-    )
-    today = datetime.now().strftime("%Y%m%d")  # 出力例: 20210601
-    targetDir = ""
-    insert = Insert(outputPath)
-    insert.insert_xbrl_dir(targetDir)
+    # ロックファイルが存在するか確認
+    if os.path.exists(lock_file):
+        print("前回のプロセスがまだ実行中です。終了します。")
+        sys.exit(0)  # 実行をスキップ
+
+    # ロックファイルを作成
+    with open(lock_file, 'w') as f:
+        f.write("")
+
+    try:
+        outputPath = "/home/kent2980/docker_cont/stock-link-parser/output"
+        today = datetime.now().strftime("%Y%m%d")  # 出力例: 20210601
+        targetDir = f"/home/kent2980/doc/tdnet/{today}"
+        insert = Insert(outputPath)
+        insert.insert_xbrl_dir(targetDir)
+
+    finally:
+        # 処理が終了したらロックファイルを削除
+        if os.path.exists(lock_file):
+            os.remove(lock_file)
