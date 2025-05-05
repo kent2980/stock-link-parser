e83f63d0121861618bf2beb76d3cbb3766d48c09
kent2980 <kent2980@gmail.com>
2025-05-05 21:42:39 +0900

    日付取得ロジックを修正し、無効な日付をスキップする処理を追加。指定されたディレクトリが存在しない場合のエラーメッセージを追加。

--- app/insert_month.py
+++ app/insert_month.py
@@ -25,30 +25,30 @@ if __name__ == "__main__":
         f.write("")
 
     try:
-        for i in range(2025, 2026):
-            for j in range(1, 12):
-                year = i
-                month = j
+        for year in range(2025, 2026):
+            for month in range(1, 12):
                 loop = True
-                for _month in range(month, 0, -1):
-                    # 指定された月の日付をループで取得
-                    for day in range(31, 0, -1):
-                        try:
-                            date = datetime.date(year, _month, day)
-                        except ValueError:
-                            # 無効な日付（例：11月31日）をスキップ
-                            continue
+                # 指定された月の日付をループで取得
+                for day in range(1,31):
+                    try:
+                        date = datetime.date(year, month, day)
+                    except ValueError:
+                        # 無効な日付（例：11月31日）をスキップ
+                        continue
 
-                        date_str = date.strftime("%Y%m%d")
-                        target_dir = f"{doc_dir}/{date.strftime("%Y年")}/{date.strftime("%m月")}/{date_str}"
-                        if os.path.exists(target_dir):
-                            try:
-                                insert = Insert(output_path, api_base_url)
-                                insert.insert_xbrl_dir(target_dir)
-                            except ApiInsertionException:
-                                continue
-                    if not loop:
-                        break
+                    date_str = date.strftime("%Y%m%d")
+                    target_dir = f"{doc_dir}/{date.strftime("%Y年")}/{date.strftime("%m月")}/{date_str}"
+                    if os.path.exists(target_dir):
+                        try:
+                            insert = Insert(output_path, api_base_url)
+                            insert.insert_xbrl_dir(target_dir)
+                        except ApiInsertionException:
+                            continue
+                    else:
+                        print(f"指定されたディレクトリが存在しません: {target_dir}")
+                        continue
+                if not loop:
+                    break
     finally:
         # 処理が終了したらロックファイルを削除
         if os.path.exists(lock_file):
